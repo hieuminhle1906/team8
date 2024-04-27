fd80fdcc6db9c3179c3cbb9b380a785584c313bd
NgoNguyenThuyLinh <150639260+NgoNguyenThuyLinh@users.noreply.github.com>
2024-04-27 23:35:54 +0700

    bth

---
+++ CMUCS246VIS_GROUP8/1. PROCESS/3.CODE/Lê Minh Hiếu/FromChinh/src/main/java/com/mycompany/fromchinh/FromChinh.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.fromchinh;
+
+/**
+ *
+ * @author Windows 10
+ */
+public class FromChinh {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
